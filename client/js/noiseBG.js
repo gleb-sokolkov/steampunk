c555fc19be39aa395491d6fcd376139da7c0d48d
gleb-sokolkov <zmihpog@gmail.com>
2021-10-04 14:58:36 +0600

    added film-postprocess

--- client/js/noiseBG.js
+++ client/js/noiseBG.js
@@ -1,6 +1,10 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
 import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
 import { fragment, vertex } from './shaders/noiseBG';
 
 function getFullScreenCorners() {
@@ -44,12 +48,26 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('noiseBG'),
   antialias: true,
 });
+const composer = new EffectComposer(renderer);
+const renderPass = new RenderPass(scene, camera);
+// renderPass.renderToScreen = true;
+
+const filmPass = new FilmPass(
+  0.05,
+  0.0,
+  1000,
+  false,
+);
+filmPass.renderToScreen = true;
+composer.addPass(renderPass);
+composer.addPass(filmPass);
 
 // --------------------------------------------------------------------------------- Window events
 window.addEventListener('resize', () => {
   [camera.left, camera.right, camera.top, camera.bottom] = getFullScreenCorners();
   camera.updateMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight, false);
+  composer.setSize(window.innerWidth, window.innerHeight);
   noiseU.resolution.value = new THREE.Vector2(window.innerWidth, window.innerHeight);
 });
 window.addEventListener('scroll', () => {
@@ -59,10 +77,10 @@ window.addEventListener('scroll', () => {
   const val = ws / (mh - wh + 0.1);
   noiseU.scrollY.value = val;
 });
-// --------------------------------------------------------------------------------- Window events
 
 window.dispatchEvent(new Event('resize'));
 window.dispatchEvent(new Event('scroll'));
+// --------------------------------------------------------------------------------- Window events
 
 const noiseQuad = new THREE.PlaneGeometry(width, height);
 const quadMaterial = new THREE.ShaderMaterial({
@@ -74,8 +92,18 @@ const quad = new THREE.Mesh(noiseQuad, quadMaterial);
 quad.position.z = -1;
 scene.add(quad);
 
+// --------------------------------------------------------------------------------- GUI
+const gui = new GUI();
+const folder = gui.addFolder('FilmPass');
+folder.add(filmPass.uniforms.grayscale, 'value').name('grayscale');
+folder.add(filmPass.uniforms.nIntensity, 'value', 0, 1).name('noise intensity');
+folder.add(filmPass.uniforms.sIntensity, 'value', 0, 1).name('scanline intensity');
+folder.add(filmPass.uniforms.sCount, 'value', 0, 1000).name('scanline count');
+// --------------------------------------------------------------------------------- GUI
+
 (function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  composer.render();
   noiseU.time.value = clock.getElapsedTime();
+
+  requestAnimationFrame(animate);
 }());
